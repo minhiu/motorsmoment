10ee16efd5720158eae2cd224454c2030e6f425c
Hieu Pham <phaminhieu.dev@gmail.com>
2022-04-14 13:02:59 -0700

    Add Coming Soon page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { Fragment } from "react/cjs/react.production.min";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Motors Moment</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header
+          title={
+            <Fragment>
+              M{<FontAwesomeIcon icon={faGear} size="xs" />}TORS MOMENT
+            </Fragment>
+          }
+        />
+        <p className="description">Coming Soon</p>
       </main>
-
-      <Footer />
     </div>
-  )
+  );
 }
